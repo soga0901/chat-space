93414e76664c8dda53fa7873cc124c9168328daf
soga0901 <sg.0901.sg@gmail.com>
2017-04-11 19:59:24 +0900

    adjustment params

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,6 +19,6 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name, { user_id: [] })
+    params.require(:group).permit(:name, user_ids: [])
   end
 end
